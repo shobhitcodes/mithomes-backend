ac9cf9044c7ba1ee6c3a73cd0785dffa0142e7d2
Shobhit Agarwal <mailshobhitnow@gmail.com>
2023-02-12 18:10:49 +0530

    Update property.js

--- models/property.js
+++ models/property.js
@@ -13,6 +13,16 @@ const propertySchema = new Schema(
         localityId: { type: mongoose.Types.ObjectId },
         locality: String,
         type: String,
+        ownershipType: String,
+        bhkType: String,
+        plotArea: Number, // in sq.ft
+        builtUpArea: Number, // in sq.ft
+        carpetArea: Number, // in sq.ft
+        propertyAge: String,
+        facing: String, // direction
+        floorType: String,
+        floor: String,
+        totalFloor: Number,
         location: { type: String },
         description: { type: String },
         availableType: { type: String },
@@ -20,6 +30,48 @@ const propertySchema = new Schema(
         active: { type: Boolean, default: true },
         deleted: { type: Boolean, default: false },
         meta: Schema.Types.Mixed, // bedroom, bathroom, car-parking
+        amenities: {
+            gym: Boolean,
+            swimmingPool: Boolean,
+            intercom: Boolean,
+            shoppingCenter: Boolean,
+            clubHouse: Boolean,
+            lift: Boolean,
+            playArea: Boolean,
+            gasPipeline: Boolean,
+            gatedSecurity: Boolean,
+            fireSafety: Boolean,
+            park: Boolean,
+            internetProvider: Boolean,
+            laundry: Boolean,
+            modularKitchen: Boolean,
+            smokeDetection: Boolean,
+        },
+        localityDetails: {
+            city: String,
+            locality: Number,
+            street: Number,
+            lat: String,
+            lng: String,
+            other: String,
+        },
+        priceDetails: {
+            expectedPrice: Number,
+            negotiable: Boolean,
+            underLoan: Boolean,
+        },
+        details: {
+            waterSupply: String,
+            bathroom: Number,
+            balcony: Number,
+            propertyShower: String,
+            powerBackup: String,
+            directionTipDesc: String,
+            kitchenType: String,
+            furnishingType: String,
+            parking: String,
+            propDesc: String,
+        },
     },
     { timestamps: true, collection: 'property' }
 );
