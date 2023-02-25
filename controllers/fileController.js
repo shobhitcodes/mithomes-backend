0c2c6d5c2b89374058a3e70ef44ee2808b45e86d
Shobhit Agarwal <mailshobhitnow@gmail.com>
2023-02-26 00:24:08 +0530

    file api changes!!

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,13 +1,1 @@
 'use strict';
-
-// const utils = require('../helpers/utils');
-// const propertyService = require('../services/propertyService');
-
-// // public interface
-// module.exports.getById = getById;
-// module.exports.getByUserId = getByUserId;
-// module.exports.getByProjectId = getByProjectId;
-// module.exports.getAll = getAll;
-// module.exports.create = create;
-// module.exports.update = update;
-// module.exports.deleteOne = deleteOne;
--- routes/file.js
+++ routes/file.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const fileController = require('../controllers/fileController');
+// const fileController = require('../controllers/fileController');
 const { auth } = require('../middlewares/auth');
 const multer = require('multer');
 // const upload = multer({ dest: 'uploads/' });
-const path = require('path');
 const File = require('../models/file');
 
 //Configuration for Multer
@@ -23,37 +22,26 @@ const upload = multer({
 });
 
 // routes
-// router.get('/', auth, propertyController.getAll);
-// router.post('/', auth, propertyController.create);
-// router.get('/:id', auth, propertyController.getById);
-// router.get('/getByUser/:id', auth, propertyController.getByUserId);
-// router.get('/getByProject/:id', auth, propertyController.getByProjectId);
-// router.put('/:id', auth, propertyController.update);
-// router.delete('/:id', auth, propertyController.deleteOne);
-
-//API Endpoint for uploading file
-router.post(
-    '/uploadFile',
-    auth,
-    upload.single('fileUpload'),
-    async (req, res) => {
-        // Stuff to be added later
-        console.log(req.file);
-        try {
-            const newFile = await File.create({
-                name: req.file.filename,
-            });
-            res.status(200).json({
-                status: 'success',
-                message: 'File created successfully!!',
-            });
-        } catch (error) {
-            res.json({
-                error,
-            });
+router.post('/uploadFile', auth, upload.any('fileUpload'), async (req, res) => {
+    try {
+        if (req.files.length) {
+            for (const file of req.files) {
+                await File.create({
+                    name: file.filename,
+                });
+            }
         }
+
+        res.status(200).json({
+            status: 'success',
+            message: 'File created successfully!!',
+        });
+    } catch (error) {
+        res.json({
+            error,
+        });
     }
-);
+});
 
 router.get('/getFiles', async (req, res) => {
     try {
