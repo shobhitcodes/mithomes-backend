0479430ec4935d009b4ef72d934baf4f45d20216
Shobhit Agarwal <mailshobhitnow@gmail.com>
2023-02-25 12:02:27 +0530

    Update file.js

--- routes/file.js
+++ routes/file.js
@@ -3,8 +3,24 @@ const router = express.Router();
 const fileController = require('../controllers/fileController');
 const { auth } = require('../middlewares/auth');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+// const upload = multer({ dest: 'uploads/' });
 const path = require('path');
+const File = require('../models/file');
+
+//Configuration for Multer
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public');
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split('/')[1];
+        cb(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
+    },
+});
+
+const upload = multer({
+    storage: multerStorage,
+});
 
 // routes
 // router.get('/', auth, propertyController.getAll);
@@ -16,9 +32,42 @@ const path = require('path');
 // router.delete('/:id', auth, propertyController.deleteOne);
 
 //API Endpoint for uploading file
-router.post('/api/uploadFile', auth, upload.single('myFile'), (req, res) => {
-    // Stuff to be added later
-    console.log(req.file);
+router.post(
+    '/api/uploadFile',
+    auth,
+    upload.single('fileUpload'),
+    async (req, res) => {
+        // Stuff to be added later
+        console.log(req.file);
+        try {
+            const newFile = await File.create({
+                name: req.file.filename,
+            });
+            res.status(200).json({
+                status: 'success',
+                message: 'File created successfully!!',
+            });
+        } catch (error) {
+            res.json({
+                error,
+            });
+        }
+    }
+);
+
+router.get('/api/getFiles', async (req, res) => {
+    try {
+        const files = await File.find();
+        res.status(200).json({
+            status: 'success',
+            files,
+        });
+    } catch (error) {
+        res.json({
+            status: 'Fail',
+            error,
+        });
+    }
 });
 
 module.exports = router;
